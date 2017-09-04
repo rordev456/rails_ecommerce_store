b5b4c135deedc83a6505313c07cc62658a2c85bb
rordev456 <devidev45@proton.me>
2017-09-03 18:43:34 -0600

    New product form, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
   
-  get 'products/show'
-
-  get 'products/index'
-
-  get 'products/new'
-
-  get 'products/edit'
-
-  get 'category/create'
-
-  get 'category/update'
-
-  get 'category/destroy'
+  resources :products, :users
+  resources :categories, :photos, only: [:create, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
