af04c737c85b59527dc3db71e57288c08317a5be
rordev456 <devidev45@proton.me>
2017-09-05 22:09:58 -0600

    Work on application structure

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,5 @@
+module SessionsHelper
+  def logged_in?
+    false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'products#index'
   
   resources :products, :users
