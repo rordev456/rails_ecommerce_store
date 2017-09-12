ebbdd9df7d8c8b9c683ee99a187c7bbaac518952
rordev456 <devidev45@proton.me>
2017-09-11 21:57:18 -0600

    Work on sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root 'products#index'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
+  resources :sessions, only: [:new, :create, :destroy]
   resources :products, :users
   resources :categories, :photos, only: [:create, :update, :destroy]
 
